6bd2bd9880fe00155d7922b96e564f3927ecdebd
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-19 21:32:42 +0100

    User Input Age Calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# Get user age and save it in a variable
+getAge = int(input('How old are you?'))
+
+# Calculate user age in 2050 with getAge input
+userAgeIn2050 = getAge + 27
+
+# Print user age in 2050
+print('in 2050, you will be', userAgeIn2050, 'years old.')
