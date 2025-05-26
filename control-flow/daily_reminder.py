fbf959bb0ebeed3c73785237ec2ce01edd304a05
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-26 15:27:15 +0100

    Adjust the logic for auto review

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,18 +3,27 @@ task = input('Enter your task: ')
 priority = input('Priority (high/medium/low): ').lower()
 time_bound = input('Is it time-bound? (yes/no):').lower()
 
-reminder = ''
+# Utilities
+err_message = "something went wrong!"
+first_part = f"{task} is a {priority} priority task"
 
 # Process the Task Based on Priority and Time Sensitivity:
 match priority:
   case 'high':
-    reminder = f'Reminder: {task} is a {priority} task'
+    reminder = 'that requires immediate attention today!'
   case 'medium':
-    reminder = f'Reminder: {task} is a {priority} task'
+    reminder = 'Consider completing it when you have free time.'
   case 'low':
-    reminder = f'Reminder: {task} is a {priority} task'
+    reminder = 'Consider completing it when you have free time.'
+  case _:
+    message = err_message
 
 if time_bound == 'yes':
-  print(f'{reminder} that requires immediate attention today!')
+  reminder = f'{first_part} that requires immediate attention today!'
+elif time_bound == "no":
+    reminder = f'{first_part} Consider completing it when you have free time.'
 else:
-  print(f'{reminder}. Consider completing it when you have free time.')
+  message = err_message
+
+print("Reminder: ", reminder)
+
