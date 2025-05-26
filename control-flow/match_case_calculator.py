e5699bbea13550f2db5c2ee84b99cafa09430639
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-26 14:17:11 +0100

    Adjust logic for auto review

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,24 +1,24 @@
 # Get user input
 num1 = int(input('Enter the first number: '))
 num2 = int(input('Enter the second number: '))
-operation_type = (input('Choose the operation (+, -, *, /): '))
-
-result = 0
+operation = (input('Choose the operation (+, -, *, /): '))
 
 # Perform calculation
-match operation_type:
-  case '+':
-   result = num1 + num2
-  case '-':
-   result = num1 - num2
-  case '*': 
-   result = num1 * num2
-  case '/':
-    if num1 and num2 != 0:
-     result = num1 / num2
-    else:
-     result = 'Cannot divide by zero'
-  case _:
-    result = 'Error: Invalid operation'
-
-print(f'The result is {result}.')
+match operation:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result is {result}.")
+        else:
+            print("Cannot divide by zero.")
+    case _:
+        print("Error: Invalid operation selected.")
