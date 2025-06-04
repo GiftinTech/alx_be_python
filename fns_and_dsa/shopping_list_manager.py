f5de7eb59fc91477fe8f143612bd7c8b1d54b1ab
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-06-04 17:58:04 +0100

    Use "" instead of  for automated test system

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,44 +13,44 @@ def main():
         choice = input("Enter your choice: ")
 
         if not choice.isdigit():
-          print('Invalid input. Please enter a number between 1 and 4.')
+          print("Invalid input. Please enter a number between 1 and 4.")
           continue
 
         choice = int(choice)
 
         if choice == 1:
             # Prompt for and add an item
-            item_name = input('Enter the to add: ')
+            item_name = input("Enter the item to add: ")
 
             if item_name:
                 if item_name in shopping_list:
-                    print(f'The {item_name} is already on the list')
+                    print(f"The {item_name} is already on the list")
                 else:
                     shopping_list.append(item_name)
-                    print(f'{item_name} added succesfully!')
+                    print(f"{item_name} added succesfully!")
             else: 
-                print('You must enter an item!')
+                print("You must enter an item!")
             pass
         
         elif choice == 2:
             # Prompt for and remove an item
-            item_name = input('Enter the item to remove: ')
+            item_name = input("Enter the item to remove: ")
 
             if item_name in shopping_list:
                 shopping_list.remove(item_name)
-                print(f'{item_name} removed successfully!')
+                print(f"{item_name} removed successfully!")
             else:
-                print(f'{item_name} not found in shopping list.')
+                print(f"{item_name} not found in shopping list.")
             pass
         
         elif choice == 3:
             # Display the shopping list
             if shopping_list != []:
-              print('Shopping list item(s):')
+              print("Shopping list item(s):")
               for item in shopping_list:
-                print(f'- {item}')
+                print(f"- {item}")
             else:
-                print('They are no items on the shopping list. Enter 1 to add an item.')
+                print("They are no items on the shopping list. Enter 1 to add an item.")
             pass
         
         elif choice == 4:
