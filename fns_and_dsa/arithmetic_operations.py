69233964390e0ca0b400f9c74b3a0f6833a33cb1
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-06-04 16:25:22 +0100

    Adjust divisible by zero logic for auto checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,11 +6,13 @@ def perform_operation(num1, num2, operation):
   elif operation == 'multiply':
     return num1 * num2
   elif operation == 'divide':
-    if num1 and num2 != 0:
-      return num1 / num2
+    if num1 == 0:
+      return ('Not divisible by zero')
+    elif num2 == 0:
+      return ('Not divisible by zero')
     else:
-      result = 'Not divisible by zero'
-      return result
+      return num1 / num2
+    
 
 
 """
