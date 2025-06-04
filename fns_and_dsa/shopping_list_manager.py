48d3603b230ca2a765d7440336d85c0bbeb52d4f
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-06-04 17:52:45 +0100

    Put the choice validation in its own block and add int()

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,47 +12,51 @@ def main():
         
         choice = input("Enter your choice: ")
 
-        if choice.isdigit():
+        if not choice.isdigit():
+          print('Invalid input. Please enter a number between 1 and 4.')
+          continue
 
-          if choice == '1':
-              # Prompt for and add an item
-              item_name = input('Enter the to add: ')
+        choice = int(choice)
 
-              if item_name:
-                  if item_name in shopping_list:
-                      print(f'The {item_name} is already on the list')
-                  else:
-                      shopping_list.append(item_name)
-                      print(f'{item_name} added succesfully!')
-              else: 
-                  print('You must enter an item!')
-              pass
-          
-          elif choice == '2':
-              # Prompt for and remove an item
-              item_name = input('Enter the item to remove: ')
+        if choice == 1:
+            # Prompt for and add an item
+            item_name = input('Enter the to add: ')
 
-              if item_name in shopping_list:
-                  shopping_list.remove(item_name)
-                  print(f'{item_name} removed successfully!')
-              else:
-                  print(f'{item_name} not found in shopping list.')
-              pass
-          
-          elif choice == '3':
-              # Display the shopping list
-              if shopping_list != []:
-                print('Shopping list item(s):')
-                for item in shopping_list:
-                  print(item)
-              else:
-                  print('They are no items on the shopping list. Enter 1 to add an item.')
-              pass
-          
-          elif choice == '4':
-              print("Exiting... Goodbye!")
-              break
+            if item_name:
+                if item_name in shopping_list:
+                    print(f'The {item_name} is already on the list')
+                else:
+                    shopping_list.append(item_name)
+                    print(f'{item_name} added succesfully!')
+            else: 
+                print('You must enter an item!')
+            pass
         
+        elif choice == 2:
+            # Prompt for and remove an item
+            item_name = input('Enter the item to remove: ')
+
+            if item_name in shopping_list:
+                shopping_list.remove(item_name)
+                print(f'{item_name} removed successfully!')
+            else:
+                print(f'{item_name} not found in shopping list.')
+            pass
+        
+        elif choice == 3:
+            # Display the shopping list
+            if shopping_list != []:
+              print('Shopping list item(s):')
+              for item in shopping_list:
+                print(f'- {item}')
+            else:
+                print('They are no items on the shopping list. Enter 1 to add an item.')
+            pass
+        
+        elif choice == 4:
+            print("Exiting... Goodbye!")
+            break
+      
         else:
             print("Invalid choice. Please enter a number between 1-4.")
 
